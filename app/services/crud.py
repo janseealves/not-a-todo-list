cd5c2e988c695a2aed4734448a1714a23b0e6803
jansee <jansen.alvesr@gmail.com>
2025-09-23 20:53:36 -0300

    Implement CRUD operations for task management

---
+++ app/services/crud.py
@@ -0,0 +1,101 @@
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from core.schemas import Task, TaskCreate, TaskUpdate
+
+
+class CreateTaskError(Exception):
+    pass
+class TaskNotFound(Exception):
+    pass
+
+class UpdateTaskError(Exception):
+    pass
+
+def create_task(db: Session, task_in: TaskCreate):
+    query = text("""
+        INSERT INTO task (title, description)
+        VALUES (:title, :description)
+        RETURNING id;
+    """)
+
+    result = db.execute(query, {
+        "title": task_in.title,
+        "description": task_in.description
+    })
+
+    row = result.fetchone()
+    if row is None: 
+        raise CreateTaskError
+    id = row[0]
+
+    db.commit()
+
+    return Task(
+        id=id,
+        title=task_in.title,
+        description=task_in.description
+    )
+
+
+def read_tasks(db: Session):
+    query = text("""
+        SELECT * 
+        FROM tasks;
+    """)
+
+    result = db.execute(query)
+    return result.fetchall()
+
+
+def read_task(db: Session, id: int):
+    query = text("""
+        SELECT *
+        FROM tasks
+        WHERE id = :id;
+    """)
+
+    result = db.execute(query, {"id": id})
+    return result.first()
+
+
+def update_task(db: Session, id: int, task_update: TaskUpdate):
+    update_data = task_update.model_dump(exclude_unset=True)
+
+    if not update_data:
+        raise UpdateTaskError
+
+    if not read_task(db, id):
+        raise TaskNotFound
+
+    set_clauses = [f"{key} = :{key}" for key in update_data.keys()]
+
+    set_clauses_string = ", ".join(set_clauses)
+
+
+    query = text(f"""
+        UPDATE tasks
+        SET 
+            {set_clauses_string}
+        WHERE id = :id
+    """)
+
+    db.execute(query, update_data)
+    db.commit()        
+
+    return read_task(db, id)
+        
+
+def delete_task(db: Session, id: int):
+    if not read_task(db, id):
+        raise TaskNotFound
+
+    # Implementar verificações...
+    query = text("""    
+        DELETE
+        FROM tasks
+        WHERE id = :id;
+    """)
+
+    db.execute(query, {"id": id})
+    db.commit()
