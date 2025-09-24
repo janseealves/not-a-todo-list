b2c026d202c7b04280c5ac6acc1b7b6ecd91f233
jansee <jansen.alvesr@gmail.com>
2025-09-24 19:01:53 -0300

    Refactor main.py to ensure database directory is created if it doesn't exist

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+import os
+
 from api.endpoints import router
 from core.database import engine
 from core.models import Base
+
+db_dir = "./db"
+if os.path.exists(db_dir):
+    os.mkdir(db_dir)
 
 # Cria as tabelas do banco de dados na inicialização da aplicação
 Base.metadata.create_all(engine)
@@ -14,4 +20,3 @@ def hello_world():
     return {"Hello": "World"}
 
 app.include_router(router)
-    
